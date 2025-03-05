0eabe6444de53a62e273ee249bc1afb3fa2c1b8f
Edwing <macdo796@gmail.com>
2025-03-05 00:20:12 -0500

    o

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 server.listen(3001, () => {
-    server.listen(port, () => {
-        console.log(`JSON Server is running on port ${port}`);
-    });
-    
+  console.log('JSON Server is running on port 3001');
 });
